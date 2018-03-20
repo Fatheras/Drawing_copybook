f9ce30b343260e329325f676ab56c0cfd91261ba
Fatheras <kosras.96@gmail.com>
2018-03-20 11:23:52 +0200

    addition_into_repository

---
+++ myscript.js
@@ -0,0 +1,13 @@
+// function that builds a grid in the "container"
+function createGrid(x) {
+    for (var rows = 0; rows < x; rows++) {
+        for (var columns = 0; columns < x; columns++) {
+            $("#container").append("<div class='grid'></div>");
+        };
+    };
+    $(".grid").width(960/x);
+    $(".grid").height(960/x);
+};
+$(document).ready(function() {
+    createGrid(16);
+    });
