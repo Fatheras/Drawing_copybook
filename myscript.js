ac5a687673df34c3fb8607dc12ebd6a416ab420c
Fatheras <kosras.96@gmail.com>
2018-03-22 09:25:58 +0200

    someChanges

--- myscript.js
+++ myscript.js
@@ -1,23 +1,31 @@
 function createGrid(x) {
-    for (var rows = 0; rows < x; rows++) {
-        for (var columns = 0; columns < x; columns++) {
-            $("#container").append("<div class='grid'></div>");
-        };
+  for (var rows = 0; rows < x; rows++) {
+    for (var columns = 0; columns < x; columns++) {
+      $("#container").append("<div class='grid'></div>");
     };
-    $(".grid").width(960/x);
-    $(".grid").height(960/x);
+  };
+  $(".grid").width(960 / x);
+  $(".grid").height(960 / x);
 };
+
 function clearGrid() {
   $('.grid').remove();
 }
+
 function refreshGrid() {
   var z = prompt("How many boxes per side do you want?");
   clearGrid();
   createGrid(z);
 }
+
 $(document).ready(function() {
-    createGrid(16);
-    $('#newGrid').click(function() {
-      refreshGrid();
-    });
-    });
+  createGrid(16);
+  $('#newGrid').click(function() {
+    refreshGrid();
+  });
+  $('.colorBoxes').click(function() {
+    var containerForBackgroundColor = $(this).css('background-color');
+    //console.log(containerForBackgroundColor);
+    document.getElementById("currentColor").style.backgroundColor=containerForBackgroundColor;
+  });
+});
